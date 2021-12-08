b5ebc8bd4fd54137e19bc8a54d4fc35a66d5743c
Jack Foltz <jack@foltz.io>
2021-12-08 14:11:59 -0500

    Remove old sketches

--- sketches/syn005/src/pipeline/mod.rs
+++
@@ -1,8 +0,0 @@
-mod phong;
-pub use phong::Phong;
-
-mod animated;
-pub use animated::{Animated, Material as AnimatedMaterial, MaterialDesc as AnimatedMaterialDesc};
-
-mod fx;
-pub use fx::{Fx, FxState};
